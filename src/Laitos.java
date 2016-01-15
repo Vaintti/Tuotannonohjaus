7707d55b8c14cf716a6b6443f961afa4a2488d7b
Vaintti <antti.jk.vainikka@gmail.com>
2016-01-15 15:05:15 +0200

    Testimetodi lisätty

--- src/Laitos.java
+++ src/Laitos.java
@@ -10,4 +10,8 @@ public class Laitos extends UnicastRemoteObject implements LaitosRajapinta{
 	public Laitos(Ruuvikuljetin a, Siilo[] siilot, Ruuvikuljetin[] b, Juomakeitin[] j, Pumppu[] p, Kypsytyssäiliö[] ks, Pumppu[] pl) throws RemoteException {
 		
 	}
+	
+	public void testi(){
+		System.out.println("Onnistunut etäkutsu");
+	}
 }
--- src/LaitosRajapinta.java
+++ src/LaitosRajapinta.java
@@ -1,5 +1,6 @@
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface LaitosRajapinta extends Remote {
-
+	public void testi() throws RemoteException;
 }
