0521ee10e48356e61e4a394898adc74ec3fcc8f7
Vaintti <antti.jk.vainikka@gmail.com>
2016-01-16 15:21:22 +0200

    Poistettu turhaa

--- src/Laitos.java
+++ src/Laitos.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 
 public class Laitos extends UnicastRemoteObject implements LaitosRajapinta{
 	
-	private ArrayList<Thread> juomamestarit = new ArrayList<Thread>();
-	
 	public Laitos(Ruuvikuljetin a, Siilo[] siilot, Ruuvikuljetin[] b, Juomakeitin[] j, Pumppu[] p, Kypsytyssäiliö[] ks, Pumppu[] pl) throws RemoteException {
 		
 	}
