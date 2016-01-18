df3ad7eb12b4015bc2042afb83ff6611863f4af2
waltton2 <viwawa@utu.fi>
2016-01-18 14:43:37 +0200

    Juomakeitin aloitus ja prosessointiaika luokka

--- src/Juomakeitin.java
+++ src/Juomakeitin.java
@@ -5,11 +5,17 @@ public class Juomakeitin {
 	private int tilavuusVesi;
 	private int tilavuusRaakaaine;
 	private int prosessointiaika;
+	
+	public Juomakeitin(){
+		tilavuusVesi = 0;
+		tilavuusRaakaaine = 0;
+		prosessointiaika = 0;
+	}
 
 	public boolean VesiTaytto(){
 		
 	}
-
+// pit‰‰ k‰ynnist‰‰ uusi thread kun alkaa prosessoida (20 000 millisekuntia)
 
 
 
---
+++ src/Prosessointiaika.java
@@ -0,0 +1,7 @@
+
+public class Prosessointiaika implements Runnable {
+	private final int aika; 
+
+	//run metodi katso mallia summaajasta(sivu26) ja hajoyhteydestä (rivi 102)
+	//dalay 20000 millisekuntia.
+}
