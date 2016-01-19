a35c8b704c43c074da9fd809063c95602aef1855
Vaintti <antti.jk.vainikka@gmail.com>
2016-01-19 07:26:00 +0200

    Jatkettu ruuvikuljetinta ja lisätty tarvittava runnable siirtoprosessia varten

--- src/Ruuvikuljetin.java
+++ src/Ruuvikuljetin.java
@@ -16,10 +16,12 @@ public class Ruuvikuljetin {
 		for(Siilo si : s) {
 			saatavilla += si.getTäyttö();
 		}
+		// Haluttu raaka-ainemäärä
 		int halutaan = 0;
 		for(Juomakeitin ju : j) {
 			// halutaan += j.getTäyttökatto()-j.getTäyttö();
 		}
+		// Lopulta siirrettävä raaka-ainemäärä
 		int siirretään = määrä;
 		if(saatavilla < siirretään) {
 			siirretään = saatavilla;
@@ -27,6 +29,12 @@ public class Ruuvikuljetin {
 		if(halutaan < siirretään) {
 			siirretään = halutaan;
 		}
-		
+		// Siirtoprosessi
+		if(siilojenTäyttö) {
+			new Thread(new Ruuvisiirto(siirretään, s));
+		}
+		else {
+			new Thread(new Ruuvisiirto(s, siirretään, j));
+		}
 	}
 }
---
+++ src/Ruuvisiirto.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Ruuvisiirto implements Runnable {
+	// Attribuutit
+	private ArrayList<Siilo> siilot = new ArrayList<Siilo>();
+	private ArrayList<Juomakeitin> juomakeittimet = new ArrayList<Juomakeitin>();
+	private int siirtom‰‰r‰;
+	
+	// Konstruktori siilot t‰ytt‰v‰lle kuljettimelle
+	public Ruuvisiirto(int m, ArrayList<Siilo> s) {
+		
+	}
+	
+	// Konstruktori juomakeittimet t‰ytt‰v‰lle kuljettimelle
+	public Ruuvisiirto(ArrayList<Siilo> s, int m, ArrayList<Juomakeitin> j) {
+		
+	}
+	
+	public void run() {
+		
+	}
+}
