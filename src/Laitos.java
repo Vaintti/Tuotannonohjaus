9748809f2fe357631e64eb903c84b390ba98cd05
waltton2 <viwawa@utu.fi>
2016-01-20 17:47:49 +0200

    Lisätty attribuutit ja juomakeitin käynnistymetodi

--- src/Laitos.java
+++ src/Laitos.java
@@ -4,15 +4,33 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
 
 public class Laitos extends UnicastRemoteObject implements LaitosRajapinta{
+	private Ruuvikuljetin ruuvi;
+	private Siilo[] siiloArray;
+	private Ruuvikuljetin[] ruuviArray;
+	private Juomakeitin[] juomakeitinArray;
+	private Pumppu [] pumppuArray1;
+	private Kypsytyssäiliö[] kypsytyssäiliöA;
+	private Pumppu [] pumppuArray2;
+	
+	
 	
 	public Laitos(Ruuvikuljetin a, Siilo[] siilot, Ruuvikuljetin[] b, Juomakeitin[] j, Pumppu[] p, Kypsytyssäiliö[] ks, Pumppu[] pl) throws RemoteException {
-		
+		ruuvi=a ;
+		siiloArray=siilot;
+		ruuviArray=b;
+		juomakeitinArray = j;
+		pumppuArray1=p;
+		kypsytyssäiliöA =ks;
+		pumppuArray2=pl;
 	}
 	
 	public void testi(){
 		System.out.println("Onnistunut etäkutsu");
 	}
 	public void juomakeitinKäynnistys(int i){
-		Juomakeitin[i].käynnistys;
+		juomakeitinArray[i].käynnistys();
+	}
+	public void siilo(int i){
+		
 	}
 }
