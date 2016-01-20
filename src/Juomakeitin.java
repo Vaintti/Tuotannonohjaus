de739ebfc659554618721bfae6d8da055b9bfd1b
waltton2 <viwawa@utu.fi>
2016-01-20 12:43:30 +0200

    Juomakeitin get set yms hauskaa

--- src/Juomakeitin.java
+++ src/Juomakeitin.java
@@ -3,17 +3,66 @@ public class Juomakeitin {
 
 	private String varaaja;
 	private int tilavuusVesi;
+	private final int VESIMAX;
 	private int tilavuusRaakaaine;
+	private final int RAAKAMAX;
 	private int prosessointiaika;
+	private boolean vesikäyttö;
+	private boolean raakakäyttö;
 	
 	public Juomakeitin(){
 		tilavuusVesi = 0;
 		tilavuusRaakaaine = 0;
+		VESIMAX = 2000;
+		RAAKAMAX = 10000;
 		prosessointiaika = 0;
+		varaaja = null;
 	}
-
-	public boolean VesiTaytto(){
-		
+	//varaajan asetus, kysely ja reset
+	public void setVaraaja(String v){
+		if(varaaja == null){
+			varaaja = v;
+		}
+	}
+	public String getVaraaja(){
+		return varaaja;
+	}
+	public void resetVaraaja(){
+		varaaja = null;
+	}
+	//veden asetus ja kysely sekä veden max tilavuuden kysely
+	public void setVesi(int v){
+		tilavuusVesi = v;
+	}
+	public int getVesi(){
+		return tilavuusVesi;
+	}
+	public int getVesiMax(){
+		return VESIMAX;
+	}
+	//raakaaineen asetus ja kysely sekä raaka-aineen max tilavuuden kysely 
+	public void setRaaka(int r){
+		tilavuusRaakaaine = r;
+	}
+	public int setRaaka(){
+		return tilavuusRaakaaine;
+	}
+	public int getRaakaMax(){
+		return RAAKAMAX;
+	}
+	//veden täyttö/tyhjennyslipun (käyttö) asetus ja kysely
+	public void setVesiKäyttö(boolean k){
+		vesikäyttö = k;
+	}
+	public boolean getVesiKäyttö(){
+		return vesikäyttö;
+	}
+	//raaka-aineen täyttö/tyhjennyslipun (käyttö) asetus ja kysely
+	public void setRaakaKäyttö(boolean k){
+		raakakäyttö = k;
+	}
+	public boolean getRaakaKäyttö(){
+		return raakakäyttö;
 	}
 // pitää käynnistää uusi thread kun alkaa prosessoida (20 000 millisekuntia)
 
