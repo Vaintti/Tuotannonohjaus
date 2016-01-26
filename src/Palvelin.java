b38906a2eee2ba1156523d9ae1a2499233fdcb24
Vaintti <antti.jk.vainikka@gmail.com>
2016-01-26 10:30:00 +0200

    asdfdfdsa

---
+++ src/Palvelin.java
@@ -0,0 +1,34 @@
+
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import tuotannonohjaus.*;
+
+public class Palvelin {
+	public static void main(String[] args){
+		// Luodaan laitoksen osat
+		Ruuvikuljetin a = new Ruuvikuljetin(true);
+		Siilo[] b = {new Siilo(), new Siilo(), new Siilo(), new Siilo()};
+		Ruuvikuljetin[] c = {new Ruuvikuljetin(false), new Ruuvikuljetin(false)};
+		Juomakeitin[] d = {new Juomakeitin(), new Juomakeitin(), new Juomakeitin()};
+		Pumppu[] e = {new Pumppu(false), new Pumppu(false)};
+		Kypsytyssäiliö[] f = new Kypsytyssäiliö[10];
+		for(int i = 0; i < 10; i++){
+			f[i] = new Kypsytyssäiliö();
+		}
+		Pumppu[] g = {new Pumppu(true), new Pumppu(true)};
+		try{
+			// Luodaan osista laitos
+			/**if(System.getSecurityManager()==null){
+				System.setSecurityManager(new RMISecurityManager());
+			}**/
+			Laitos laitos = new Laitos(a, b, c, d, e, f, g);
+			Registry registry = LocateRegistry.createRegistry(2020);
+			Naming.rebind("//localhost:2020/Laitos", laitos);
+			System.out.println("Serveri käynnistetty.");
+		}catch(Exception expe){
+			System.out.println("Error: "+expe);
+		}
+	}
+}
