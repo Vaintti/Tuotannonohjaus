3f71eca632f3ddc6b8e5c33751cf1d2a3f4127b2
Vaintti <antti.jk.vainikka@gmail.com>
2016-01-29 12:52:34 +0200

    Lisätty palautukset rajapintaan käyttöliittymää varten

--- src/tuotannonohjaus/LaitosRajapinta.java
+++ src/tuotannonohjaus/LaitosRajapinta.java
@@ -4,14 +4,14 @@ import java.rmi.RemoteException;
 
 public interface LaitosRajapinta extends Remote {
 	public void testi() throws RemoteException;
-	public void juomakeitinKäynnistys(int i) throws RemoteException;
-	public void startKeittimienTäytin(int kuljetin, int määrä, String[] käyttäjä) throws RemoteException;
-	public void varaaSiilo(int siilo, String[] käyttäjä) throws RemoteException;
-	public void varaaKeitin(int keitin, String[] käyttäjä) throws RemoteException;
-	public void käynnistäKeitin(int keitin, String[] käyttäjä) throws RemoteException;
-	public void käynnistäPumppu(int pumppu, String[] käyttäjä) throws RemoteException;
-	public void käynnistäPullotusPumppu(int pumppu, String[] käyttäjä) throws RemoteException;
-	public void varaaSäiliö(int säiliö, String[] käyttäjä) throws RemoteException;
+	public boolean juomakeitinKäynnistys(int i) throws RemoteException;
+	public boolean startKeittimienTäytin(int kuljetin, int määrä, String[] käyttäjä) throws RemoteException;
+	public boolean varaaSiilo(int siilo, String[] käyttäjä) throws RemoteException;
+	public boolean varaaKeitin(int keitin, String[] käyttäjä) throws RemoteException;
+	public boolean käynnistäKeitin(int keitin, String[] käyttäjä) throws RemoteException;
+	public boolean käynnistäPumppu(int pumppu, String[] käyttäjä) throws RemoteException;
+	public boolean käynnistäPullotusPumppu(int pumppu, String[] käyttäjä) throws RemoteException;
+	public boolean varaaSäiliö(int säiliö, String[] käyttäjä) throws RemoteException;
 	public String[] login(String nimi) throws RemoteException;
 	public void logout(String[] id) throws RemoteException;
 	public Laitos update() throws RemoteException;
